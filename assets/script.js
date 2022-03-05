5bf127120a9ea5158f41919ef8e770cb3eb84831
Emma Malone <emalias2509@gmail.com>
2022-03-04 22:16:52 -0500

    added content to script.js and timeslots in index.html

---
+++ assets/script.js
@@ -0,0 +1,16 @@
+var current = moment().hour();
+
+$("#currentDay").text(moment().format("LLL"));
+
+$(".row").each(function (){
+    var hour = $(this).data("hour");
+    var message = localStorage.getItem(hour);
+    $(this).find(".description").text(message);
+    $(this).addClass( hour > current ? "future" : hour < current ? "past" : "present");
+
+});
+
+$(".row").on("click",".saveBtn", function(){
+    var result = $(this).parent().find(".description").val();
+    localStorage.setItem($(this).parent().data("hour"), result);
+});
